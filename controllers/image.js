c7edf95fae1d49a3574d77f1b80759095246c0c8
Jacob <jacobdeansd@gmail.com>
2019-08-08 12:10:29 -0700

    UPDATE

--- controllers/image.js
+++ controllers/image.js
@@ -1,31 +1,70 @@
 const Clarifai = require('clarifai');
-
-//You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+   apiKey: process.env.API_CLARIFAI
 });
 
-const handleApiCall = (req, res) => {
-  app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => res.status(400).json('unable to work with API'))
+var multer = require('multer');
+var storage = multer.diskStorage({
+  filename: function(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+var imageFilter = function (req, file, cb) {
+    // accept image files only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+        return cb(new Error("Only image files are allowed!"), false);
+    }
+    cb(null, true);
+};
+var upload = multer({ storage: storage, fileFilter: imageFilter})
+
+
+const cloudinary = require("cloudinary");
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const handleImageUpload = () => (req, res) => {
+	console.log(req.files);
+	const values = Object.values(req.files);
+  const promises = values.map(image => cloudinary.uploader.upload(image.path));
+  
+  Promise
+    .all(promises)
+    .then(results => res.json(results));
 }
 
-const handleImage = (req, res, db) => {
-  const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
+function escapeRegex(text) {
+	return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 }
 
-module.exports = {
-  handleImage,
-  handleApiCall
+const handleApiCall = () => (req, res) => {
+	app.models.predict("a403429f2ddf4b49b307e318f00e528b", req.body.input)
+	.then(data => {
+		res.json(data);
+	})
+	.catch(err => res.status(400).json(-1));
 }
+
+
+const handleImage = (db) => (req, res) => {
+	const {id} = req.body;
+
+	db("users")
+		.where("id", "=", id)
+		.increment("entry", 1)
+		.returning("entry")
+	.then( entry => {
+		if(entry.length){
+			res.json(entry);
+		} else {
+			res.status(400).json("unable to get entries of user with id = " + id);
+		}
+	})
+	.catch( err => res.status(400).json("unable to get entries"));
+}
+
+module.exports = {handleImage, handleApiCall, handleImageUpload}
--- server.js
+++ server.js
@@ -1,34 +1,48 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
+require("dotenv").config();
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+const formData = require('express-form-data');
 
-const db = knex({
-  client: 'pg',
-  connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true,
-  }
-});
+const register = require("./controllers/register.js");
+const signin = require("./controllers/signin.js");
+const profile = require("./controllers/profile.js");
+const image = require("./controllers/image.js");
 
 const app = express();
-
-app.use(cors())
 app.use(bodyParser.json());
+app.use(cors());
+app.use(formData.parse());
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+const db = knex({
+	client: 'pg',
+	connection: {
+		connectionString: process.env.DATABASE_URL,
+		ssl: true
+	}
+});
 
-app.listen(process.env.PORT || 3000, ()=> {
-  console.log(`app is running on port ${process.env.PORT}`);
-})
+app.get("/", (req, res) => {
+	res.send("welcome");
+});
+
+app.post("/signin", signin.handleSignIn(db, bcrypt));
+
+app.post("/register", register.handleRegister(db, bcrypt));
+
+app.get("/profile/:id", profile.handleProfile(db));
+
+app.put("/image", image.handleImage(db));
+app.post("/imageurl", image.handleApiCall());
+app.post("/image-upload", image.handleImageUpload());
+
+app.get("*", (req, res) => {
+	res.send("Sorry, nothing to find here");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+	console.log("server starts");
+});
