a35568c4d98283d041f2d9075a160f97a7ee315f
Jacob <jacobdeansd@gmail.com>
2019-08-08 16:12:46 -0700

    typo

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,26 +1,27 @@
-const handleSignin = (db, bcrypt) => (req, res) => {
-	const { email, password } = req.body;
-	if (!email || !password) {
-	  return res.status(400).json('incorrect form submission');
+const handleSignIn = (db, bcrypt) => (req, res) =>  {
+	const {email, password} = req.body;
+	if( !email ||  !password){
+		return res.status(400).json("incorrect data");
 	}
+
 	db.select('email', 'hash').from('login')
-	  .where('email', '=', email)
-	  .then(data => {
+    .where('email', '=', email)
+	.then(data => {
 		const isValid = bcrypt.compareSync(password, data[0].hash);
-		if (isValid) {
-		  return db.select('*').from('users')
-			.where('email', '=', email)
+		if(isValid){
+			return db.select("*").from("users")
+			.where("email", "=", email)
 			.then(user => {
-			  res.json(user[0])
+				res.json(user[0]);
 			})
-			.catch(err => res.status(400).json('unable to get user'))
+			.catch(err => res.status(400).json("error logging in"))
 		} else {
-		  res.status(400).json('wrong credentials')
+			res.status(400).json("wrong credentions");
 		}
-	  })
-	  .catch(err => res.status(400).json('wrong credentials'))
-  }
-  
-  module.exports = {
-	handleSignin: handleSignin
-  }
+	})
+	.catch(err => res.status(400).json("wrong credentions"));
+}
+
+module.exports = {
+	handleSignIn: handleSignIn
+}
