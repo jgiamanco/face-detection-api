9140252c64f2b891c9aa03fc389bc65b2623ac32
Jacob <jacobdeansd@gmail.com>
2019-08-08 16:41:55 -0700

    typo

--- server.js
+++ server.js
@@ -26,8 +26,54 @@ const db = knex({
 });
 
 app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+app.post('/signin', (req, res) => {
+	db.select('email', 'hash').from('login')
+	  .where('email', '=', req.body.email)
+	  .then(data => {
+		const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+		if (isValid) {
+		  return db.select('*').from('users')
+			.where('email', '=', req.body.email)
+			.then(user => {
+			  res.json(user[0])
+			})
+			.catch(err => res.status(400).json('unable to get user'))
+		} else {
+		  res.status(400).json('wrong credentials')
+		}
+	  })
+	  .catch(err => res.status(400).json('wrong credentials'))
+  })
+  
+  app.post('/register', (req, res) => {
+	const { email, name, password } = req.body;
+	const hash = bcrypt.hashSync(password);
+	  db.transaction(trx => {
+		trx.insert({
+		  hash: hash,
+		  email: email
+		})
+		.into('login')
+		.returning('email')
+		.then(loginEmail => {
+		  return trx('users')
+			.returning('*')
+			.insert({
+			  email: loginEmail[0],
+			  name: name,
+			  joined: new Date()
+			})
+			.then(user => {
+			  res.json(user[0]);
+			})
+		})
+		.then(trx.commit)
+		.catch(trx.rollback)
+	  })
+	  .catch(err => res.status(400).json('unable to register'))
+  })
+  
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post("/imageUrl", image.handleApiCall());
